dd2aefdc594ab5da437b66028876c024099ce8e6
Austin Burrow <austin.burrow@intouchsol.com>
2013-07-15 15:28:28 -0500

    Initial Commit

---
+++ ITSDemoTests/ITSDemoTests.m
@@ -0,0 +1,36 @@
+//
+//  ITSDemoTests.m
+//  ITSDemoTests
+//
+//  Created by Austin Burrow on 7/15/13.
+//  Copyright (c) 2013 Intouch Solutions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ITSDemoTests : XCTestCase
+
+@end
+
+@implementation ITSDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
