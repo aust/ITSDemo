374ec26fdd1c75bdf7df207e29ef6680fb7659ee
Austin Burrow <austin.burrow@intouchsol.com>
2013-07-15 15:46:44 -0500

    Added main storyboard and removed default window initialization from AppDelegate

---
+++ ITSDemo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  ITSDemo
+//
+//  Created by Austin Burrow on 7/15/13.
+//  Copyright (c) 2013 Intouch Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
